739feb4f33d6b9d477eed15bd18fd098eaeefa3b
YonatanBenezra <yonatanbenezra1@gmail.com>
2021-11-29 00:07:04 +0200

    before deploy

---
+++ README.md
@@ -0,0 +1,14 @@
+# A portfolio built for Dimitry Pomazan
+
+A blazing fast photography portfolio built with Gatsby
+
+https://V1eos-world.netlify.com
+
+## Features
+
+- Near 100 Lighthouse scores 🏆
+- Automated CI/CD ⚙️
+- SEO optimized 🕵️‍♂️
+- Static hosting ☁️
+- Blazing fast 🔥
+- PWA 💣
---
+++ config/site.js
@@ -0,0 +1,25 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'V1eos', // Navigation and Site Title
+  siteTitleAlt: 'V1eos - Photography', // Alternative Site title for SEO
+  siteTitleShort: 'V1eos', // short_name for manifest
+  siteHeadline: 'V1eos', // Headline for schema.org JSONLD
+  siteUrl: 'https://V1eos-world.netlify.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo.png', // Used for SEO and manifest
+  siteDescription: 'V1eos - Personal photography portfolio',
+  author: 'V1eos', // Author for schema.org JSONLD
+  ogLanguage: 'en_US', // og:language
+  googleAnalyticsID: 'UA-134925752-3',
+
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Your information
+  avatar: '/logos/logo.png',
+  name: 'V1eos',
+  location: 'United states',
+  socialMedia: [],
+}
